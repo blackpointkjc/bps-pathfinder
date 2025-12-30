597c3b482756da4c7f83d33319233a069cd962bb
Admin Virtus Security <sahpdocs@gmail.com>
2025-12-30 17:25:09 +0000

    File changes

--- src/components/map/VolunteerFireCompanies.jsx
+++ src/components/map/VolunteerFireCompanies.jsx
@@ -7,7 +7,7 @@ const vfcIcon = new L.Icon({
     iconUrl: 'data:image/svg+xml;base64,' + btoa(`
         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" width="32" height="32">
             <circle cx="16" cy="16" r="15" fill="#EF4444" stroke="white" stroke-width="2"/>
-            <path d="M22 22c.55 0 1-.45 1-1s-.45-1-1-1-1 .45-1 1 .45 1 1 1zm1-6h-2v2.5h3l-1-2.5zM10 22c.55 0 1-.45 1-1s-.45-1-1-1-1 .45-1 1 .45 1 1 1zm13-10l2 2.5v3.5h-1.5c0 1.38-1.12 2.5-2.5 2.5s-2.5-1.12-2.5-2.5h-6c0 1.38-1.12 2.5-2.5 2.5S7.5 19.88 7.5 18.5H6v-8h11v2h2zm-11-1h-2v3h2v-3z" fill="white"/>
+            <path d="M16 7c-1.5 2.5-3 4.5-3 7 0 1.66 1.34 3 3 3s3-1.34 3-3c0-2.5-1.5-4.5-3-7zm0 11c-2.21 0-4-1.79-4-4 0-3 2-5.5 4-8.5 2 3 4 5.5 4 8.5 0 2.21-1.79 4-4 4zm-1 1h2v6h-2v-6z" fill="white"/>
         </svg>
     `),
     iconSize: [32, 32],
