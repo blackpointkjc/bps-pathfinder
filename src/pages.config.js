2c0abbbc5123465eee23f60c7d2cecc106e491ed
Admin Virtus Security <sahpdocs@gmail.com>
2025-12-30 05:35:41 +0000

    File changes

--- src/pages.config.js
+++ src/pages.config.js
@@ -1,6 +1,5 @@
 import AdminPortal from './pages/AdminPortal';
 import CallHistory from './pages/CallHistory';
-import Dispatch from './pages/Dispatch';
 import DispatchCenter from './pages/DispatchCenter';
 import Navigation from './pages/Navigation';
 
@@ -8,7 +7,6 @@ import Navigation from './pages/Navigation';
 export const PAGES = {
     "AdminPortal": AdminPortal,
     "CallHistory": CallHistory,
-    "Dispatch": Dispatch,
     "DispatchCenter": DispatchCenter,
     "Navigation": Navigation,
 }
