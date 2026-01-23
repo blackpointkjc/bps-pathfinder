fd62a67da76619e37887a671ee3cde96d068a655
kavonhiers <kavonhiers@team-hsg.com>
2026-01-23 00:08:38 +0000

    File changes

--- functions/fetchAllUsers.ts
+++ functions/fetchAllUsers.ts
@@ -10,19 +10,20 @@ Deno.serve(async (req) => {
         }
 
         // Use service role to fetch all users
-        const allUsers = await base44.asServiceRole.entities.User.list('-last_updated', 200);
+        const allUsers = await base44.asServiceRole.entities.User.list('created_date', 200);
         
-        // Filter to only include users with valid location data
-        const users = allUsers.filter(u => {
-            return u.latitude && u.longitude && 
-                   !isNaN(u.latitude) && !isNaN(u.longitude) &&
-                   u.latitude !== 0 && u.longitude !== 0;
-        });
+        // Return all users for roster, but mark which have valid location data
+        const users = allUsers.map(u => ({
+            ...u,
+            hasValidLocation: !!(u.latitude && u.longitude && 
+                               !isNaN(u.latitude) && !isNaN(u.longitude) &&
+                               u.latitude !== 0 && u.longitude !== 0)
+        }));
         
         return Response.json({
             success: true,
             users: users,
-            total: allUsers.length
+            total: users.length
         });
         
     } catch (error) {
